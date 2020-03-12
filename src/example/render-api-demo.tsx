b2ca18649a6bb2b53dcda4a17fce05b1b1552cf3
lxj <1073830130@qq.com>
2020-03-12 20:46:43 +0800

    fix: 流程调整

--- src/example/render-api-demo.tsx
+++ src/example/render-api-demo.tsx
@@ -1,19 +1,26 @@
 import * as React from 'react';
-import ReactDom from 'react-dom';
-import { getPortalsNode } from '@lxjx/utils';
 
-import renderApi, { DemoComponent } from './demo-component';
+import renderApi from './demo-component';
+
+function render1(num: number) {
+  const [ref, id] = renderApi({
+    name: 'lxj',
+    age: 17,
+    // singleton: true,
+  });
+
+  console.log(num, ref, id);
+}
+
+for (let i = 0;i<10;i++) {
+  render1(i);
+}
+
 
 const RenderApiDemo = () => {
 
   React.useEffect(() => {
-    const [ref, id] = renderApi({
-      name: 'lxj',
-      age: 17,
-      // singleton: true,
-    });
-
-    console.log(2, ref, id);
+    render1(4);
   });
 
   return (
@@ -46,3 +53,23 @@ const RenderApiDemo = () => {
 };
 
 export default RenderApiDemo;
+
+// function tempFn(methodKey, ...arg) {
+//   console.log('延迟调用1', ref.current)
+//   setTimeout(() => {
+//     if (ref.current) {
+//       console.log('延迟调用2', ref.current)
+//       ref.current[methodKey](...arg);
+//     } else {
+//       tempFn(methodKey, ...arg);
+//     }
+//   }, 50);
+// }
+//
+// const temp = {
+//   close(id){ tempFn('close', id) },
+//   closeAll(){ tempFn('closeAll') },
+//   remove(removeId){ tempFn('remove', removeId) },
+//   removeAll(){ tempFn('removeAll') },
+//   update(id, newProps) { tempFn('update', id, newProps) },
+// }
